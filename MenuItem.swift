a2b162d51c6081c39fd0aca99791a7c028cf5ca8
Gharbin <bg2696@nyu.edu>
2024-09-19 00:06:26 +0400

    Add files via upload

---
+++ MenuItem.swift
@@ -0,0 +1,17 @@
+//
+//  MenuItem.swift
+//  Menu App
+//
+//  Created by Gharbin Bernard on 04/09/2024.
+//
+
+import Foundation
+
+struct MenuItem: Identifiable{
+	
+	var id: UUID =  UUID()
+	var name: String
+	var price: String
+	var imageName: String
+	
+}
---
+++ Menu_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Menu_AppApp.swift
+//  Menu App
+//
+//  Created by Gharbin Bernard on 04/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Menu_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MenuView()
+        }
+    }
+}
